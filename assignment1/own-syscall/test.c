8070ed3fd455385e87d79cad821b0ac0730e18f5
Kaustubh Welankar <kaustubhwelankar@gmail.com>
2018-09-07 18:32:58 +0530

    No need of syscall(int)

--- assignment1/own-syscall/test.c
+++ assignment1/own-syscall/test.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define MY_FORK 363
-void myFork(void){
-    syscall(MY_FORK);
-}
-
 int main(){
     myFork();
     printf("This should be printed twice\n");
